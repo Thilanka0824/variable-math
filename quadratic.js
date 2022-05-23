817bba75a8ee59ae0ff24a30a9b271d7afde5052
Thilanka0824 <frontleanrest24@gmail.com>
2022-05-22 19:40:50 -0700

    vari-math and quadratic formula

---
+++ quadratic.js
@@ -0,0 +1,9 @@
+let a = 2
+let b = 9
+let c = 3
+
+let x1 = (-b + Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+let x2 = (-b - Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+
+console.log(x1)
+console.log(x2)
