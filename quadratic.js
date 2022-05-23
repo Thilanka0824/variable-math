7e51b617107e447b35f7fd13b24982e5563c52b9
Thilanka0824 <frontleanrest24@gmail.com>
2022-05-22 19:53:14 -0700

    vari-math and quadratic formula

--- quadratic.js
+++ quadratic.js
@@ -5,5 +5,5 @@ let c = 3
 let x1 = (-b + Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
 let x2 = (-b - Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
 
-console.log(x1)
-console.log(x2)
+console.log("The value of x1 is: " + x1)
+console.log("The value of x2 is: " + x2)
